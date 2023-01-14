5ba33fd93ab3bd5831759ded7975f32879fe5b92
Lea Tinoso <l.tinoso@damtratechnology.com>
2023-01-15 02:57:23 +0800

    Adds functionilty to send message

--- state-management-chat/src/SendMessage.jsx
+++ state-management-chat/src/SendMessage.jsx
@@ -1,22 +1,44 @@
 import "./App.css";
+import React, { useState } from "react";
 
-function SendMessage() {
-  return (
-    <div>
-    <form className="input-group">
-      <input
-        type="text"
-        className="form-control"
-        placeholder="Enter your message..."
-      />
-      <div className="input-group-append">
-        <button className="btn submit-button">
-          SEND
-        </button>
-      </div>
-    </form>
+function SendMessage({onAddMessage}) {
+ const [message, setMessage] = useState({
+  username: "",
+  text: "",
+ });
+
+ const handleInputChange = (event) => {
+  const { name, value } = event.target;
+  setMessage({ ...message, [name]: value });
+ };
+
+ // If the user did not type anything, he/she should not be allowed to submit.
+ const isDisabled = () => {
+  return message.text === "";
+ };
+
+ const handleSubmit = (event) => {
+  event.preventDefault();
+
+  onAddMessage(message);
+ };
+
+ return (
+  <div>
+   <form className='input-group' onSubmit={handleSubmit}>
+    <input
+     type='text'
+     className='form-control'
+     placeholder='Enter your message...'
+     name='text'
+     onChange={handleInputChange}
+    />
+    <div className='input-group-append'>
+     <button className='btn submit-button' disabled={isDisabled()}>SEND</button>
+    </div>
+   </form>
   </div>
-  );
+ );
 }
 
 export default SendMessage;
--- state-management-chat/src/User.jsx
+++ state-management-chat/src/User.jsx
@@ -1,28 +1,27 @@
 import "./App.css";
 import Messages from "./Messages";
 import SendMessage from "./SendMessage";
+import React, { useState } from 'react';
 
-const messages = [
-  { username: "Franklin", text: "Hi, Michael!" },
-  { username: "Franklin", text: "How are you?" },
-  { username: "Michael", text: "Hi, Franklin! Good, you?" },
- ];
-
-const createMessage = (message) => {};
-
-// If the user did not type anything, he/she should not be allowed to submit.
-const isDisabled = () => {
-  return false;
-};
 
 function User({ username }) {
+  const [messages, setMessages] = useState([
+    { username: "Franklin", text: "Hi, Michael!" },
+    { username: "Franklin", text: "How are you?" },
+    { username: "Michael", text: "Hi, Franklin! Good, you?" },
+  ]);
+
+  const createMessage = (message) => {
+    message.username = username;
+    setMessages([...messages, message])
+  };
  return (
   <>
    <div className='chat-window'>
     <h2>Chat</h2>
     <div className='name sender'>{username}</div>
     <Messages messages={messages} username={username} />
-    <SendMessage/>
+    <SendMessage onAddMessage={createMessage}/>
    </div>
   </>
  );
